a826b86659c5739d05e397aa2c2049f3012eef77
Niko Alvarez <alvarez.niko.angeles@gmail.com>
2024-10-18 17:03:36 +0800

    appdev1

--- cont/App.js
+++ cont/App.js
@@ -4,23 +4,14 @@ import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 export default function NavigationBar() {
   return (
     <View style={styles.container}>
-      {/* Top Navigation Bar */}
       <View style={styles.topNav}>
         <Image style={styles.logo} source={require('./assets/logo.png')} />
         <Text style={styles.text}>PHYSICAL EDUCATION</Text>
         <Image style={styles.searchIcon} source={require('./assets/search1.png')} />
       </View>
-      {/* Line for top navigation */}
       <View style={styles.topLine} />
-
-      {/* Content Area */}
-      <View style={styles.contentArea}>
-        {/* Place for your main content */}
-      </View>
-
-      {/* Bottom Navigation Bar Container */}
+      <View style={styles.contentArea} />
       <View style={styles.bottomNavContainer}>
-        {/* Line for bottom navigation */}
         <View style={styles.bottomLine} />
         <View style={styles.bottomNav}>
           <TouchableOpacity onPress={() => {}}>
@@ -40,14 +31,14 @@ export default function NavigationBar() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, // Ensures the container takes full height
+    flex: 1,
     backgroundColor: 'white',
   },
   topNav: {
     flexDirection: 'row',
     alignItems: 'center',
     paddingHorizontal: 16,
-    marginTop: 45,  // Margin at the top of the top navigation bar
+    marginTop: 45,
     paddingBottom: 3,
   },
   logo: {
@@ -69,37 +60,27 @@ const styles = StyleSheet.create({
     resizeMode: 'contain',
     marginLeft: 35,
   },
-  
-  // Line styles for top navigation
   topLine: {
     height: 2,
     backgroundColor: 'rgba(128, 128, 128, 0.2)',
-    marginTop: 8, // Space between top navigation and line
-    marginHorizontal: '5%', // Ensure space at the sides
+    marginTop: 8,
+    marginHorizontal: '5%',
   },
-  
-  /* Line for Bottom Navigation */
   bottomLine: {
     height: 2,
     backgroundColor: 'rgba(128, 128, 128, 0.2)',
-    marginHorizontal: '5%', // Ensure space at the sides
+    marginHorizontal: '5%',
   },
-
-  /* Content Area */
   contentArea: {
-    flex: 1, // Allows the content area to take up the available space between top and bottom navs
+    flex: 1,
   },
-
-  /* Bottom Navigation Bar Container */
   bottomNavContainer: {
-    position: 'absolute', // Position it at the bottom
-    bottom: 0, // Align to the bottom
-    left: 0, // Align to the left edge
-    right: 0, // Align to the right edge
-    backgroundColor: 'white', // Ensure the background is white
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'white',
   },
-  
-  /* Bottom Navigation Bar Styles */
   bottomNav: {
     height: 60,
     flexDirection: 'row',
